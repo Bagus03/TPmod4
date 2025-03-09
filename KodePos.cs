02a99c564d91b43af2b6e80fc742455d85cccef7
Bagus Hardiyanto <bagushardiyanto03@gmail.com>
2025-03-09 18:02:46 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmod4
+{
+    class KodePos
+    {
+        // Dictionary untuk menyimpan data kelurahan dan kode pos
+        private Dictionary<string, int> tabelKodePos = new Dictionary<string, int>()
+        {
+            { "Batununggal", 40266 },
+            { "Kujangsari", 40287 },
+            { "Mengger", 40267 },
+            { "Wates", 40256 },
+            { "Cijaura", 40287 },
+            { "Jatisari", 40286 },
+            { "Margasari", 40286 },
+            { "Sekejati", 40286 },
+            { "Kebonwaru", 40272 },
+            { "Maleer", 40274 },
+            { "Samoja", 40273 }
+        };
+
+        // Method untuk mendapatkan kode pos berdasarkan kelurahan
+        public int? GetKodePos(string kelurahan)
+        {
+            if (tabelKodePos.ContainsKey(kelurahan))
+            {
+                return tabelKodePos[kelurahan];
+            }
+            else
+            {
+                return null; // Mengembalikan null jika kelurahan tidak ditemukan
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TPmod4
+{
+    class Program
+    {
+        static void Main()
+        {
+            KodePos kodePos = new KodePos();
+
+            // Meminta input dari user
+            Console.Write("Masukkan nama kelurahan: ");
+            string kelurahan = Console.ReadLine();
+
+            // Mendapatkan kode pos dari kelurahan
+            int? kodePosResult = kodePos.GetKodePos(kelurahan);
+
+            // Menampilkan hasilnya
+            if (kodePosResult != null)
+            {
+                Console.WriteLine($"Kode pos untuk {kelurahan} adalah: {kodePosResult}");
+            }
+            else
+            {
+                Console.WriteLine("Kelurahan tidak ditemukan.");
+            }
+        }
+    }
+}
